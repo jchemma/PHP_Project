db189e71398c3b53fa66941c850f025c25df5760
GautamRavi <gautam_ravichandran@yahoo.com>
2017-03-01 16:49:18 -0500

    Battle.php update
    Structure of battle functions updated

--- php/Battle.php
+++ php/Battle.php
@@ -2,25 +2,35 @@
 
 $hero = new player(20, 50, 3, 8);
 $dragon = new enemy(70, 3, 10);
+$heroattack;
+$enemmyattack;
+
+public function roll(){
+	$heroattack = hero->attack();
+	$enemyattack = enemy->attack();
+}
+
+public function reroll(){
+	$heroattack = hero->attack();
+}
 
 public function battle(){
-	if($hero->attack > $enemy->attack){
-		$enemy->health -= ($hero->attack - $enemy->attack)
+	if($heroattack > $enemyattack){
+		$enemy->health -= ($heroattack - $enemyattack)
 	}
 
-	if($hero->attack < $enemy->attack){
-		$hero->health -= ($enemy->attack - $health->attack)
+	if($heroattack < $enemyattack){
+		$hero->health -= ($enemyattack - $healthattack)
 	}
 
-	if($hero->defense < $enemy->attack){
-		$hero->health -= ($enemy->attack - $health->attack)
+	if($herodefense < $enemyattack){
+		$hero->health -= ($enemyattack - $healthattack)
 	}
 	
-	if($hero->defense > $enemy->attack){
+	if($hero->defense > $enemyattack){
 		//nothing yet;
 	}
 }
 
-public function reroll(){
-}
+
 ?>
