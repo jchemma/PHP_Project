650ddc410989f23ffc3748bffc85a56d6b5bb0a8
GautamRavi <gautam_ravichandran@yahoo.com>
2017-03-01 15:08:09 -0500

    php code
    updated with constructors and attack and defense

---
+++ php/Character.php
@@ -0,0 +1,40 @@
+<?php
+class superCharacter{
+	protected $health;
+	protected $atk;
+	protected $def;
+	
+	functin __construct($x,$y,$z){
+		$health = $x;
+		$atk = $y;
+		$def = $z;
+	}
+	public function attack(){
+		public $total = 0;
+		for($i=0; $i<$atk; $i++){
+			$total += int rand(1,6);
+		}
+	}
+	
+}
+
+class player extends superCharacter{
+	private $mana;
+	
+	function __construct($w, $x, $y, $z){
+		parent:: __construct($x, $y, $z);
+		$mana = $w;
+	}
+	
+	public function defend(){
+		public $total = 0;
+		for($i=0; $i<def; $i++){
+			$total += int rand(1,6);
+		}
+	}
+}
+
+class enemy extends superCharacter{
+
+}
+?>
