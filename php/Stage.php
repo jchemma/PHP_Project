8fd89ecba06b2330a632f71bf8aadad834210f27
Jim Chemmalakuzh <chemmalakuzhyj@gmail.com>
2017-03-03 22:06:28 -0500

    Removed html

--- php/Stage.php
+++ php/Stage.php
@@ -6,31 +6,29 @@
 </head>
 <body>
 	<?php
-		include("Character.php");
-		
 		$hero = new player(20, 50, 3, 8);
 		$dragon = new enemy(70, 3, 10);
 		$heroattack;
 		$enemmyattack;
 		
-		public function roll(){
+		function rollAtk(){
 			$heroattack = hero->attack();
 			$enemyattack = enemy->attack();
 		}
-
+		public function rollDef(){
+			$heroattack = hero->defend();
+			$enemyattack = enemy->attack();
+		}
 		public function reroll(){
 			$heroattack = hero->attack();
 		}
-
 		public function battle(){
 			if($heroattack > $enemyattack){
 				$enemy->health -= ($heroattack - $enemyattack)
 			}
-
 			if($heroattack < $enemyattack){
 				$hero->health -= ($enemyattack - $healthattack)
 			}
-
 			if($herodefense < $enemyattack){
 				$hero->health -= ($enemyattack - $healthattack)
 			}
